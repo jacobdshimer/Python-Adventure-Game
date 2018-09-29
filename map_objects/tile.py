8d34992976dffed25fbae74f2d69f10ee811dc76
jacobdshimer <31100766+jacobdshimer@users.noreply.github.com>
2018-09-29 14:27:23 -0400

    Added in Game Map, Entities
    Added the following functions and files
    render_functions
    - render_all
    - clear_all
    - render entity
    - clear entity
    
    game_map class
    tile map class

---
+++ map_objects/tile.py
@@ -0,0 +1,16 @@
+class Tile:
+    """
+    A tile on a map.
+    Has the following properties:
+        - Blocked - Means its solid
+        - Black Sight - Means the character can't see through it
+    """
+
+    def __init__(self, blocked, block_sight=None):
+        self.blocked = blocked
+
+        # By default, if a tile is blocked, it also blocks sight
+        if block_sight is None:
+            block_sight = blocked
+
+        self.block_sight = block_sight
