fbeaf833c5aee33a1f264f96f402ccf906d89b4b
jacobdshimer <31100766+jacobdshimer@users.noreply.github.com>
2018-10-02 13:48:35 -0400

    Added in inventory and healing function

--- input_handlers.py
+++ input_handlers.py
@@ -1,6 +1,20 @@
+# Python Imports
 import libtcodpy as libtcod
 
-def handle_keys(key):
+# Package Imports
+from game_states import GameStates
+
+def handle_keys(key, game_state):
+    if game_state == GameStates.PLAYER_TURN:
+        return handle_player_turn_keys(key)
+    elif game_state == GameStates.PLAYER_DEAD:
+        return handle_player_dead_keys(key)
+    elif game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
+        return handle_inventory_keys(key)
+
+    return {}
+
+def handle_player_turn_keys(key):
     key_char = chr(key.c)
     #Movement keys
     if key.vk == libtcod.KEY_UP or key_char == 'w':
@@ -21,6 +35,46 @@ def handle_keys(key):
     if key_char == 'c':
         return {'move':(1, 1)}
 
+    if key_char == 'x':
+        return {'pickup': True}
+
+    elif key_char == 'i':
+        return {'show_inventory': True}
+
+    elif key_char == 'r':
+        return {'drop_inventory': True}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        #Set fullscreen
+        return {'fullscreen': True}
+
+    elif key.vk == libtcod.KEY_ESCAPE:
+        #Exit the game
+        return {'exit': True}
+
+    #Nothing was pressed
+    return {}
+
+def handle_player_dead_keys(key):
+    key_char = chr(key.c)
+    if key_char == 'i':
+        return {'show_inventory': True}
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        #Set fullscreen
+        return {'fullscreen': True}
+
+    elif key.vk == libtcod.KEY_ESCAPE:
+        #Exit the game
+        return {'exit': True}
+
+    #Nothing was pressed
+    return {}
+
+def handle_inventory_keys(key):
+    index = key.c - ord('a')
+
+    if index >= 0:
+        return {'inventory_index': index}
     if key.vk == libtcod.KEY_ENTER and key.lalt:
         #Set fullscreen
         return {'fullscreen': True}
