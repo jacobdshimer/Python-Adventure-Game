c9f7084ce8f4dfcf73cdc1cd301fad93e53845a5
jacobdshimer <31100766+jacobdshimer@users.noreply.github.com>
2018-10-02 21:49:24 -0400

    Added in two new spells!

--- input_handlers.py
+++ input_handlers.py
@@ -9,6 +9,8 @@ def handle_keys(key, game_state):
         return handle_player_turn_keys(key)
     elif game_state == GameStates.PLAYER_DEAD:
         return handle_player_dead_keys(key)
+    elif game_state == GameStates.TARGETING:
+        return handle_targeting_keys(key)
     elif game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
         return handle_inventory_keys(key)
 
@@ -55,6 +57,12 @@ def handle_player_turn_keys(key):
     #Nothing was pressed
     return {}
 
+def handle_targeting_keys(key):
+    if key.vk == libtcod.KEY_ESCAPE:
+        return {'exit': True}
+
+    return {}
+
 def handle_player_dead_keys(key):
     key_char = chr(key.c)
     if key_char == 'i':
@@ -84,4 +92,14 @@ def handle_inventory_keys(key):
         return {'exit': True}
 
     #Nothing was pressed
+    return {}
+
+def handle_mouse(mouse):
+    (x, y) = (mouse.cx, mouse.cy)
+
+    if mouse.lbutton_pressed:
+        return {'left_click': (x, y)}
+    elif mouse.rbutton_pressed:
+        return {'right_click': (x, y)}
+
     return {}
